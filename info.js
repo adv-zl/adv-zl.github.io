ad051f92fdcb6a5116384152d78aaf6f0287df74
adv-zl <nexmptest@gmail.com>
2017-02-19 00:16:41 +0200

    Update info.js

--- info.js
+++ info.js
@@ -1,1 +1,35 @@
-{"isShown":true,"articles":[{"keyword":"Проверка ключей номер 1","url":"http://ya.ru","picture":"http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg","text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"},{"keyword":"Проверка предложений","url":"http://google.ru","picture":"http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg","text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"}]}
+{  
+   "isShown":true,
+   "articles":[  
+      {  
+         "keyword":"Длинный кейворд Длинный кейворд Длинный кейворд Длинный кейворд",
+         "url":"http://ya.ru",
+         "picture":"http://sovets.net/photos/uploads/120/5604101-24.jpg",
+         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      },
+      {  
+         "keyword":"Вышивка крестиком схемы +по клеточкам легкие",
+         "url":"http://google.ru",
+         "picture":"http://cdn01.ru/files/users/images/31/06/3106ca0ed2c1c95064179dcfade71a2c.jpg",
+         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      },
+      {  
+         "keyword":"Проверка предложений 1 ",
+         "url":"http://google.ru",
+         "picture":"http://cdn01.ru/files/users/images/43/69/436901c2976dca3865b61b473a6dcea3.jpg",
+         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      },
+      {  
+         "keyword":"Проверка предложений 2 ",
+         "url":"http://google.ru",
+         "picture":"http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg",
+         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      },
+      {  
+         "keyword":"Проверка предложений 3",
+         "url":"http://google.ru",
+         "picture":"http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg",
+         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      }
+   ]
+}
