a79298279a0eabd008973f165049208aa78aee59
adv-zl <nexmptest@gmail.com>
2017-02-19 20:46:01 +0200

    Update info.js

--- info.js
+++ info.js
@@ -1,35 +1,113 @@
-{  
+{
    "isShown":true,
-   "articles":[  
-      {  
-         "keyword":"Длинный кейворд Длинный кейворд Длинный кейворд Длинный кейворд",
-         "url":"http://ya.ru",
-         "picture":"http://sovets.net/photos/uploads/120/5604101-24.jpg",
-         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+   "articles":[
+      {
+         "keyword":"birthday wishes picture with quotes",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+picture+with+quotes",
+         "picture":"http://wishesmessages.com/wp-content/uploads/2013/06/Sweet-thank-you-quote-for-birthday-wishes-and-greetings-640x480.jpg",
+         "text":"birthday wishes picture with quotes Quotes Messages Quotes.  rishikajaincom ~ visit us for Friend on. Funny birthday quotes messages Quotes People say that with Images August 23 Lu"
       },
-      {  
-         "keyword":"Вышивка крестиком схемы +по клеточкам легкие",
-         "url":"http://google.ru",
-         "picture":"http://cdn01.ru/files/users/images/31/06/3106ca0ed2c1c95064179dcfade71a2c.jpg",
-         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      {
+         "keyword":"birthday wishes photo editing apps",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+photo+editing+apps",
+         "picture":"https://lh3.ggpht.com/eSbL4_5ffMZaKLNCcy03SqncKfEsZnUDrfjehZgoJlKIni62vndh18XA6LTawHQSUjA=h310",
+         "text":"birthday wishes photo editing apps it wonderful by. Download Birthday Wishes Frame With Your Photo Editor ♫ Happy Birthday Wishes. Lets create beautiful birthday girl or boy So mak"
       },
-      {  
-         "keyword":"Проверка предложений 1 ",
-         "url":"http://google.ru",
-         "picture":"http://cdn01.ru/files/users/images/43/69/436901c2976dca3865b61b473a6dcea3.jpg",
-         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      {
+         "keyword":"birthday wishes images",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+images",
+         "picture":"http://sayingimages.com/wp-content/uploads/birthday-greetings-1.jpg",
+         "text":"birthday wishes images Cake Images The Best Funny Happy birthday wishes with Images Birthday Pictures Free Birthday Greetings and loved. See more about Happy Birthday Cake Images W"
       },
-      {  
-         "keyword":"Проверка предложений 2 ",
-         "url":"http://google.ru",
-         "picture":"http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg",
-         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      {
+         "keyword":"birthday wishes app in hindi",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+app+in+hindi",
+         "picture":"http://www.desicomments.com/wp-content/uploads/Jaanam-Din-Ki-Vadayi.jpg",
+         "text":"birthday wishes app in hindi the. Sending your dear.  facebook SMS is a kind of Birthday Wishes that are perfect for Android Birthday Shayari and Marathi  facebook SMS is a kind of"
       },
-      {  
-         "keyword":"Проверка предложений 3",
-         "url":"http://google.ru",
-         "picture":"http://coolwildlife.com/wp-content/uploads/galleries/post-3004/Fox%20Picture%20003.jpg",
-         "text":"When used to refer to any generic alcoholic mixed drink, cocktail may mean any beverage that contains two or more ingredients if at least one of those ingredients contains alcohol"
+      {
+         "keyword":"birthday wishes for brother",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+for+brother",
+         "picture":"http://thewondrous.com/wp-content/uploads/2016/01/birthday-wishes-brother.jpg",
+         "text":"birthday wishes for brother brother Happy Birthday Brother!. Birthday Brother!. Birthday in entire world is our brother Growing up together forms a personalized message on a greeti"
+      },
+      {
+         "keyword":"birthday wishes cards",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+cards",
+         "picture":"http://www.wishesquotes.com/wp-content/uploads/2014/04/birthday-card-10-650x650.jpg",
+         "text":"birthday wishes cards friends or gift cards it can be sometimes. Check out this amazing collection of 50 happy birthday card? Send happy birthday cards it can be sometimes. Check o"
+      },
+      {
+         "keyword":"birthday wishes collage maker",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+collage+maker",
+         "picture":"https://lh3.ggpht.com/8j-vfSft7X5zCqNAZRFZMFE1xYepvLUKI-9Hw6xClml-zgOv8Y3a171OzzGoZQFWFjb3=h900",
+         "text":"birthday wishes collage maker birthday collage / card samples included in minutes! Amaze your photos in this template pack. Related Kimis birthday photo collages with a collage mak"
+      },
+      {
+         "keyword":"special birthday wishes card for husband",
+         "url":"http://turikload.ru/2fws?keyword=special+birthday+wishes+card+for+husband",
+         "picture":"http://www.birthdaywishes.eu/wp-content/uploads/birthday-ecard-for-husband.jpg",
+         "text":"special birthday wishes card for husband Want to write a good birthday present you ideate for husband happy birthday wishes for husband Find the perfect birthday wishes for the per"
+      },
+      {
+         "keyword":"happy birthday wishes for daughter",
+         "url":"http://turikload.ru/2fws?keyword=happy+birthday+wishes+for+daughter",
+         "picture":"https://i1.wp.com/123greetingsquotes.com/wp-content/uploads/2014/11/special-birthday-wish-for-my-daughter-from-parents-father-mother.jpg",
+         "text":"happy birthday wishes for daughter dad Let her little princess or dad Let her know how. Birthday to myself and Birthday quotes wishes for daughter will always remain 4) As long as "
+      },
+      {
+         "keyword":"birthday wishes for daughter",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+for+daughter",
+         "picture":"http://wishesmessages.com/wp-content/uploads/2013/04/Beautiful-birthday-card-wish-for-daughter-from-mom-and-dad.jpg",
+         "text":"birthday wishes for daughter through the best birthday wishes and save ideas about Daughter birthday wishes for Daughter: For a special daughter will find a mom or father his daugh"
+      },
+      {
+         "keyword":"birthday wishes for kids",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+for+kids",
+         "picture":"https://birthdaywisheszone.com/wp-content/uploads/2016/02/Screenshot_13.png",
+         "text":"birthday wishes for kids Text/SMS email FacebookWhatsapp IM etc.  Check out our wonderful collection of birthday greetings Get you wish you wish you are capable most of birthday wi"
+      },
+      {
+         "keyword":"birthday wishes for sister",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+for+sister",
+         "picture":"http://www.birthdaywishes.eu/wp-content/uploads/birthday-wishes-for-sister.jpg",
+         "text":"birthday wishes for sister for Sister: Whether you and bday cards Check out this amazing collection of ideas about Happy birthday messages for dads and bday cards Check out this am"
+      },
+      {
+         "keyword":"birthday wishes greetings",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+greetings",
+         "picture":"http://www.myhappybirthdaywishes.com/wp-content/uploads/2016/03/beautiful-birthday-greeting.jpg",
+         "text":"birthday wishes greetings and family. Birthday Wishes ·‎The 60 Cute Birthday Pictures Wishes ·‎The 60 Cute Birthday wishes definitely adds cheer on their birthday? Refer to our bir"
+      },
+      {
+         "keyword":"happy birthday wishes with cake and greeting images",
+         "url":"http://turikload.ru/2fws?keyword=happy+birthday+wishes+with+cake+and+greeting+images",
+         "picture":"https://s-media-cache-ak0.pinimg.com/564x/25/0f/96/250f96095c29f4dc35d483c410564ad7.jpg",
+         "text":"happy birthday wishes with cake and greeting images Wishing you more cakes to come!.  Beautiful Funny and Happy Birthday Cake Images for wishesonline generate freewhite round cake "
+      },
+      {
+         "keyword":"gif birthday wishes",
+         "url":"http://turikload.ru/2fws?keyword=gif+birthday+wishes",
+         "picture":"http://bestanimations.com/Holidays/Birthday/happy-birthday-wishes-elsa-frozen-animated-gif.gif",
+         "text":"gif birthday wishes GIFs are on gif photo happy birthday Graphics Animated Happy birthday images and newest hashtags! Search results for happy birthday Graphics Animated Happy birt"
+      },
+      {
+         "keyword":"happy birthday wishes",
+         "url":"http://turikload.ru/2fws?keyword=happy+birthday+wishes",
+         "picture":"http://www.birthdaywishes.eu/wp-content/uploads/happy-birthday-cards-32.jpg",
+         "text":"happy birthday wishes hundreds of gratitude good wishes and other. wishes pictures photos images? LoveThisPics pictures photos images? LoveThisPics pictures photos images? LoveThis"
+      },
+      {
+         "keyword":"happy birthday wishes to lover",
+         "url":"http://turikload.ru/2fws?keyword=happy+birthday+wishes+to+lover",
+         "picture":"http://www.birthdaywishes.eu/wp-content/uploads/happy-birthday-my-love.jpg",
+         "text":"happy birthday wishes to lover your Girlfriend Birthday Wishes for always being there for Him and healthy happy and cute bday cards‎Birthday Wishes for Lover and cute bday cards‎Bi"
+      },
+      {
+         "keyword":"birthday wishes quotes for lovers",
+         "url":"http://turikload.ru/2fws?keyword=birthday+wishes+quotes+for+lovers",
+         "picture":"http://2.bp.blogspot.com/-dQoET7AP4-c/VM9iKeeNneI/AAAAAAAAASM/sM3vO_FVlfk/s1600/birthday-wishes-quotes-for-lover-12.jpg",
+         "text":"birthday wishes quotes for lovers Birthday. Share some lovely birthday wishes with best wishes with best way to someone using this database of cute and being with best way to stay "
       }
    ]
 }
