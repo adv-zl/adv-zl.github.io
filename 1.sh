ae1b7082629d08ef51d2e1c3d2d4847d7ddde160
adv-zl <nexmptest@gmail.com>
2017-10-20 20:45:01 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -40,7 +40,7 @@ fi
 ## now loop through the above array
 for (( i=1; i<${arraylength}+1; i++ ));
 do
-  ( 
+  #( 
     gcloud compute --project $PROJECT_ID instances create "instance-$i" --zone ${array[$i-1]} --machine-type "n1-standard-1" --subnet "default" --maintenance-policy "MIGRATE" --service-account "$PROJECT_NUM-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --image "debian-9-stretch-v20170829" --image-project "debian-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "instance-$i"
     gcloud beta compute ssh "instance-$i" --zone ${array[$i-1]} --quiet --command "sudo wget -O socks.sh https://raw.githubusercontent.com/adv-zl/adv-zl.github.io/master/dante.sh && sudo chmod +x socks.sh && ./socks.sh" 
     
@@ -49,7 +49,7 @@ do
     #gcloud compute --project $PROJECT_ID instances create "instance-$i" --zone ${array[$i-1]} --machine-type "n1-standard-8" --subnet "default" --maintenance-policy "MIGRATE" --service-account "$PROJECT_NUM-compute@developer.gserviceaccount.com" --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --image "debian-9-stretch-v20170829" --image-project "debian-cloud" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "instance-$i"
    # gcloud beta compute ssh "instance-$i" --zone ${array[$i-1]} --command "sudo apt-get install screen git unzip build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev automake -y && wget https://github.com/OhGodAPet/cpuminer-multi/archive/master.zip && unzip master.zip && cd ./cpuminer-multi-master && ./autogen.sh && CFLAGS=\"-march=native\" ./configure && make && nohup ./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u englandnancy09@gmail.com -p x -t 14 2>&1 && exit" --ssh-flag="-f"
      # gcloud beta compute ssh "instance-$i" --zone ${array[$i-1]} --quiet --command "sudo apt-get install git unzip tmux build-essential cmake libuv1-dev -y && wget https://github.com/xmrig/xmrig/archive/master.zip && unzip master.zip && cd ./xmrig-master && mkdir build && cd build && cmake .. && make && wget https://raw.githubusercontent.com/adv-zl/adv-zl.github.io/master/config.json && tmux new -d ./xmrig" 
-      ) &
+      #) &
 
    # or do whatever with individual element of the array
 done
