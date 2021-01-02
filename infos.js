1b215b28bb5a9c74a4e0524cc68a1e850eb87f45
adv-zl <nexmptest@gmail.com>
2021-01-02 23:23:35 +0200

    Update infos.js

--- infos.js
+++ infos.js
@@ -1,1 +1,1 @@
-{"product_version":"0.2", "url": "https://r.goaffmy.com/click?pid=9320&offer_id=2426"}
+{"product_version":"0.2", "url": "https://yzbqd.shewantsyou.net/c/da57dc555e50572d?s1=419&s2=1166388&j1=1&j3=1	"}
