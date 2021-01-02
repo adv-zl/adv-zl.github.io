a1f0ea5000e6d2aa873cace7aaffb205581cdc00
adv-zl <nexmptest@gmail.com>
2021-01-02 23:23:53 +0200

    Update infos.js

--- infos.js
+++ infos.js
@@ -1,1 +1,1 @@
-{"product_version":"0.2", "url": "https://yzbqd.shewantsyou.net/c/da57dc555e50572d?s1=419&s2=1166388&j1=1&j3=1	"}
+{"product_version":"0.2", "url": "https://yzbqd.shewantsyou.net/c/da57dc555e50572d?s1=419&s2=1166388&j1=1&j3=1"}
